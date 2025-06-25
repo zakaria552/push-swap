3a3444e06763ec017af8cf3d9f45e163fc5c4593
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-25 11:37:04 +0300

    added prototypes

--- include/push_swap.h
+++ include/push_swap.h
@@ -13,4 +13,14 @@ int	cracked_atoi(const char *nptr, int *error);
 // parser
 void parse(int argc, char **args, int **nums);
 
+// solver
+void	solve(int argc, int **nums);
+
+// bubble sort
+void bubble_sort(int *nums, int size);
+int get_index(int *nums, int size, int num);
+
+// radix sort
+void	radix_sort(int argc, unsigned int *norm_nums, int *sorted_nums);
+
 #endif
