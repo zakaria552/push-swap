91d9de85eba410fad80edaa01b6b052813d948f2
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-26 12:09:34 +0300

    moved crcked_atoi inside the parser

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,7 @@
 #include "push_swap.h"
 static void stratoi(int argc, char **args, int **nums);
 static int is_duplicate(int *nums, int num, int stop);
+static int	cracked_atoi(const char *nptr, int *error);
 
 void parse(int argc, char **args, int **nums)
 {
@@ -38,4 +39,33 @@ static int is_duplicate(int *nums, int num, int stop)
 			return (1);
 	}
 	return (0);
+}
+
+static int	cracked_atoi(const char *nptr, int *error)
+{
+	int	i;
+	long	num;
+	int	sign;
+
+	i = num = 0;
+	sign = 1;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = sign * (-1);
+		i++;
+	}
+	while (nptr[i] && nptr[i] <= '9' && nptr[i] >= '0')
+	{
+		num = num * 10 + nptr[i] - '0';
+		i++;
+	}
+	if (nptr[i] || num * sign > INT_MAX || num * sign < INT_MIN)
+	{
+		*error = 1;	
+		return 0;
+	}
+	return (int)(num * sign);
 }
