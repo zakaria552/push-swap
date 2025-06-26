6cddcb801f44a25cda5bcad7186eb6e81f26292e
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-26 12:11:19 +0300

    added 42 header and norm fixes

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bubble_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 12:10:08 by zfarah            #+#    #+#             */
+/*   Updated: 2025/06/26 12:10:59 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void bubble_sort(int *nums, int size)
+void	bubble_sort(int *nums, int size)
 {
-	int i;
-	int j;	
-	int tmp;
+	int	i;
+	int	j;
+	int	tmp;
 
-	i = j = -1;
+	i = -1;
 	while (++i < size)
 	{
 		j = 0;
 		while ((j + 1) < (size - i))
 		{
-			if(nums[j] > nums[j + 1]) 
+			if (nums[j] > nums[j + 1])
 			{
 				tmp = nums[j];
 				nums[j] = nums[j + 1];
@@ -19,16 +30,15 @@ void bubble_sort(int *nums, int size)
 			}
 			j++;
 		}
-
 	}
 }
 
-int get_index(int *nums, int size, int num)
+int	get_index(int *nums, int size, int num)
 {
-	int i;
+	int	i;
 
 	i = -1;
-	while(++i < size)
+	while (++i < size)
 	{
 		if (nums[i] == num)
 			return (i);
