2e89e358a546bb1064afcb2b2d5741729f37bfe1
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-26 12:37:31 +0300

    fixed norm error and added 42 header

--- src/parser.c
+++ src/parser.c
@@ -1,9 +1,23 @@
-#include "push_swap.h"
-static void stratoi(int argc, char **args, int **nums);
-static int is_duplicate(int *nums, int num, int stop);
-static int	cracked_atoi(const char *nptr, int *error);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 12:36:48 by zfarah            #+#    #+#             */
+/*   Updated: 2025/06/26 12:37:04 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void parse(int argc, char **args, int **nums)
+#include "push_swap.h"
+
+static void	stratoi(int argc, char **args, int **nums);
+static int	is_duplicate(int *nums, int num, int stop);
+static int	cracked_atoi(const char *nptr, int *error);
+static void	set_sign(const char *nptr, int *sign, int *index);
+
+void	parse(int argc, char **args, int **nums)
 {
 	*nums = ft_calloc(argc - 1, sizeof(int));
 	if (!*nums)
@@ -11,26 +25,29 @@ void parse(int argc, char **args, int **nums)
 	stratoi(argc, args, nums);
 }
 
-static void stratoi(int argc, char **args, int **nums)
+static void	stratoi(int argc, char **args, int **nums)
 {
-	int error;
-	int i;
-	int j;
+	int	error;
+	int	i;
+	int	j;
 
-	i = j = error = 0;
+	i = 0;
+	j = 0;
+	error = 0;
 	while (++i < argc)
 	{
 		(*nums)[j] = cracked_atoi(args[i], &error);
 		if (error)
 			clean_exit(nums);
-		if(is_duplicate(*nums, (*nums)[j], j))
+		if (is_duplicate(*nums, (*nums)[j], j))
 			clean_exit(nums);
 		j++;
 	}
 }
-static int is_duplicate(int *nums, int num, int stop)
+
+static int	is_duplicate(int *nums, int num, int stop)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < stop)
@@ -41,22 +58,28 @@ static int is_duplicate(int *nums, int num, int stop)
 	return (0);
 }
 
+static void	set_sign(const char *nptr, int *sign, int *index)
+{
+	if (nptr[*index] == '-' || nptr[*index] == '+')
+	{
+		if (nptr[*index] == '-')
+			*sign = (*sign) * (-1);
+		(*index)++;
+	}
+}
+
 static int	cracked_atoi(const char *nptr, int *error)
 {
-	int	i;
 	long	num;
-	int	sign;
+	int		sign;
+	int		i;
 
-	i = num = 0;
+	i = 0;
+	num = 0;
 	sign = 1;
 	while (ft_isspace(nptr[i]))
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign = sign * (-1);
-		i++;
-	}
+	set_sign(nptr, &sign, &i);
 	while (nptr[i] && nptr[i] <= '9' && nptr[i] >= '0')
 	{
 		num = num * 10 + nptr[i] - '0';
@@ -64,8 +87,8 @@ static int	cracked_atoi(const char *nptr, int *error)
 	}
 	if (nptr[i] || num * sign > INT_MAX || num * sign < INT_MIN)
 	{
-		*error = 1;	
-		return 0;
+		*error = 1;
+		return (0);
 	}
-	return (int)(num * sign);
+	return ((int)(num * sign));
 }
