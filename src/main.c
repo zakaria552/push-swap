c2f5fb05ecba8b457767a709d2bc1fb90dac3372
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-26 12:12:54 +0300

    added 42 header and norm fixes

--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 12:12:13 by zfarah            #+#    #+#             */
+/*   Updated: 2025/06/26 12:12:47 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int main(int argc, char **args)
+int	main(int argc, char **args)
 {
-	int *nums;
+	int	*nums;
 
 	nums = NULL;
 	if (argc < 3)
-		return 0;
+		return (0);
 	parse(argc, args, &nums);
 	solve(argc, &nums);
 	free(nums);
-	return 0;
+	return (0);
 }
